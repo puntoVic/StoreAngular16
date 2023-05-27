d892af55189a1574302a85a2cdbf2d2dc9710a26
Victor Olvera <volvera.thome@gmail.com>
2023-05-26 22:19:02 -0600

    primeros pasos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'my-store';
+  name = 'Vico';
+  age = 35;
+  img ='https://source.unsplash.com/random'
 }
